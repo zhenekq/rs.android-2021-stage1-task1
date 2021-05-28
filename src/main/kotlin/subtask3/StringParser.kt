f52f0deaa3f4de1112a616423e99bcc611ff219b
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-05-28 15:07:11 +0300

    add: completed third subtask

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,40 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val openBrackets = listOf('<', '[', '(')
+        val resSet = mutableSetOf<String>()
+
+        var tmp: Char
+        var str = StringBuilder()
+        var index = 0
+        var returnInd = -1
+        var repeats = 0
+
+        while (index < inputString.length) {
+            if (inputString[index] in openBrackets) {
+                tmp = inputString[index]
+                returnInd = index + 1
+                index++
+                while (inputString[index] != closeAnalog(tmp) || repeats !=0) {
+                    str.append(inputString[index])
+                    if(inputString[index] == tmp) repeats++
+                    if(inputString[index] == closeAnalog(tmp)) repeats--
+                    index++
+                }
+                resSet.add(str.toString())
+                str.clear()
+                index = returnInd
+            }
+            else index++
+        }
+
+        return resSet.toTypedArray()
+    }
+
+    private fun closeAnalog(ch: Char): Char = when (ch) {
+        '[' -> ']'
+        '<' -> '>'
+        '(' -> ')'
+        else -> ' '
     }
 }
