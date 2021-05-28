3bead4f2cab541c0821ceae7a5b4baf0940dfb15
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-05-28 03:44:18 +0300

    add: completed first subtask

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,21 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val resultArray = sadArray.toMutableList()
+
+        var pointOfExit = false
+        for (i in 1 until sadArray.size - 1) {
+            if (resultArray[i - 1] + resultArray[i + 1] < resultArray[i]) {
+                pointOfExit = true
+                resultArray.removeAt(i)
+                break
+            }
+        }
+        if(pointOfExit){
+            return convertToHappy(resultArray.toIntArray())
+        }
+        return resultArray.toIntArray()
     }
+
+
 }
